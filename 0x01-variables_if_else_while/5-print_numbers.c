686465d0aa00de4a988bc319df7d82595c0243a8
atrevorMatovu <atrevormatovu@gmail.com>
2023-02-20 08:55:05 -0800

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - main function
+ *
+ * Return: always(0) (success)
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 0 ; x < 10 ; x++)
+		printf("%d\n", x);
+	return (0);
+}
