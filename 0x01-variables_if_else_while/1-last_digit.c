bb89e5d0c02337b88e93f4a293bd610f39db4be8
atrevorMatovu <atrevormatovu@gmail.com>
2023-02-20 04:37:23 -0800

    Last digit using modulus comparison

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+/*
+ * main - Entry point
+ * main - main function
+ * Return: always(0) (success)
+ */
+
+/* betty style doc for function main goes there */
+/*main - main function*/
+int main(void)
+{
+	int n;
+	int x;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	x = n % 10;
+	if (x > 5)
+		printf("Last digit of %d is %d and is greater than 5", n, x);
+	else if (x == 0)
+		printf("Last digit of %d is %d and is 0", n, x);
+	else if (x < 6 && x != 0)
+		printf("Last digit of %d is %d and is less than 6 and not 0", n, x);
+	printf("\n");
+	return (0);
+}
