77531c68ce221886caf25138b2c9b0bd49a3caeb
atrevorMatovu <atrevormatovu@gmail.com>
2023-02-24 04:06:32 -0800

    Numbers are life

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_number - prints an integer
+ * @n: the number to be output
+ */
+void print_number(int n)
+{
+	if (n < 0 || n > 0)
+	{
+		_putchar(n + '0');
+	}
+	else
+	{
+		_putchar(n + '0');
+	}
+	_putchar('\n');
+}
