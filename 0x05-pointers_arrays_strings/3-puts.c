878b11e457f7824c4907a038f6b04f6971014216
atrevorMatovu <atrevormatovu@gmail.com>
2023-02-27 00:25:48 -0800

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string
+ * @str: character
+ */
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; str[i]; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
