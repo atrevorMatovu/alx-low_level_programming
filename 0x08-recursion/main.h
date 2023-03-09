0879dbcbcc3ae53849c8c9d7071883ef321fc9d1
atrevorMatovu <atrevormatovu@gmail.com>
2023-03-08 23:01:38 -0800

    Is it really palindrome

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -13,5 +13,7 @@ int _sqrt_recursion(int n);
 int is_prime_number(int n);
 int isqrt(int n, int i);
 int is_prime_number(int n);
-
+int is_palindrome(char *s);
+int wildcmp(char *s1, char *s2);
+int ispalindrome(char *s, int i, int j);
 #endif
